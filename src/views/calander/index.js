4ce793d0014b443dfc5a7dbfafa572e92fe59062
aliHussainSalsoft6253 <ali.talha3358@gmail.com>
2023-07-21 15:37:59 +0500

    calander

--- src/views/calander/index.js
+++ src/views/calander/index.js
@@ -1,7 +1,72 @@
-import React from "react";
-import { Col, Row, Typography, Layout,Checkbox, Card } from "antd";
+import React,{useState,useRef} from "react";
+import { Col, Row, Typography,Button, Layout, Checkbox, Card,DatePicker } from "antd";
+import AliceCarousel from 'react-alice-carousel';
+import 'react-alice-carousel/lib/alice-carousel.css';
+
+import {FcPrevious,FcNext} from "react-icons/fc"
+import dayjs from 'dayjs';
+
+
+const { RangePicker } = DatePicker;
+
+
+const responsive = {
+  0: { items: 2 },
+  568: { items: 4 },
+  1024: { items: 7 },
+};
+
+
+
 
 function Calander() {
+  const [currentIndex, setCurrentIndex] = useState(0);
+  const [selectedMonthRange, setSelectedMonthRange] = useState([dayjs(), dayjs().add(15, 'day')]);
+
+  const disabledDate = (current) => {
+    return current && current.isBefore(dayjs().endOf('day'));
+  };
+
+
+  const carouselRef = useRef();
+
+  const handleOnSlideChanged = (event) => {
+    setCurrentIndex(event.item);
+  };
+
+
+  const handlePrevSlide = () => {
+    if (carouselRef.current) {
+      const prevIndex = currentIndex - 7;
+      carouselRef.current.slideTo(prevIndex >= 0 ? prevIndex : 0);
+    }
+  };
+
+  const handleNextSlide = () => {
+    if (carouselRef.current) {
+      const nextIndex = currentIndex + 7;
+      carouselRef.current.slideTo(nextIndex < dateObjects.length ? nextIndex : dateObjects.length - 1);
+    }
+  };
+
+  const handleRangePickerChange = (dates) => {
+    setSelectedMonthRange(dates);
+  };
+
+  const generateDateObjects = (startDate, endDate) => {
+    let currentDate = startDate;
+    const dateObjects = [];
+    while (currentDate.isBefore(endDate) || currentDate.isSame(endDate, 'day')) {
+      dateObjects.push(currentDate);
+      currentDate = currentDate.add(1, 'day');
+    }
+    return dateObjects;
+  };
+
+  const dateObjects = generateDateObjects(selectedMonthRange[0], selectedMonthRange[1]);
+
+
+
   return (
     <Layout style={{ minHeight: "80vh" }}>
       <Row
@@ -44,226 +109,85 @@ function Calander() {
                 padding: "20px",
               }}
             >
-              <Typography.Title
-                className="fontFamily1"
-                style={{
-                  fontSize: "12px",
-                  fontWeight: 600,
-                  color: "black",
-                  textAlign: "left",
-                  marginTop: 0,
-                }}
-              >
+      
 
-                   {"< 15 May, 2023 - 21 May, 2023 >"}
+              <RangePicker
+          picker="month"
+          value={selectedMonthRange}
+          onChange={handleRangePickerChange}
+          disabledDate={disabledDate}
+          format="DD MMM, YYYY"
+          renderExtraFooter={() => ` < ${selectedMonthRange[0].format('DD MMM, YYYY')} - ${selectedMonthRange[1].format('DD MMM, YYYY')} >`}
+      />
 
-              </Typography.Title>
-              <br/>
-              <div>
-                <Row style={{border:"1px solid #dadada",justifyContent:'center', borderRadius:"10px"}}>
-                    <Col xs={3} style={{padding:20, display:'flex',justifyContent:'center',flexDirection:'column',alignItems:'center',borderRight:"1px solid #dadada"}}>
-                    <Typography.Title
-                className="fontFamily1"
-                style={{
-                  fontSize: "12px",
-                  fontWeight: 600,
-                  color: "black",
-                  textAlign: "left",
-                  marginTop: 0,
-                  marginBottom: 0,
-                }}
-              >
-                   Monday
-              </Typography.Title>
-              <Typography.Text
-                className="fontFamily1"
-                style={{
-                  fontSize: "12px",
-              
-                  color: "black",
-                  textAlign: "left",
-                  marginTop: 0,
-                }}
-              >
-                   March 22, 2023
-              </Typography.Text>
-                    </Col>
+<br />
+<br />
+<Row className="flex" style={{border:'1px solid #dadada',borderRadius:'10px'}}>
+<Col xs={0} md={1} className="flex">
 
-                    <Col xs={3} style={{padding:20, display:'flex',justifyContent:'center',flexDirection:'column',alignItems:'center', borderRight:"1px solid #dadada"}}>
-                    <Typography.Title
-                className="fontFamily1"
-                style={{
-                  fontSize: "12px",
-                  fontWeight: 600,
-                  color: "black",
-                  textAlign: "left",
-                  marginTop: 0,
-                  marginBottom: 0,
-                }}
-              >
-                   Tuesday
-              </Typography.Title>
-              <Typography.Text
-                className="fontFamily1"
-                style={{
-                  fontSize: "12px",
-              
-                  color: "black",
-                  textAlign: "left",
-                  marginTop: 0,
-                }}
-              >
-                   March 23, 2023
-              </Typography.Text>
-                    </Col>
+<Button shape="circle" className="flex" onClick={handleNextSlide}>
+<FcPrevious style={{fontWeight:'bold'}}/>
+        </Button>
+</Col>
+  <Col xs={24} md={22}>
+  <AliceCarousel
+          mouseTracking
+          disableButtonsControls
+          disableDotsControls
+          responsive={responsive}
+          ref={carouselRef}
+          controlsStrategy="alternate"
+          onSlideChanged={handleOnSlideChanged}
+        >
+        {dateObjects.map((date, index) => (
+         <div style={{width:"100%",height:"80px",borderRight:'1px solid #dadada',display:'flex',flexDirection:'column',justifyContent:'center',alignItems:'center' }}>
+          <Typography.Title
+              className="fontFamily1"
+              style={{
+                fontSize: '12px',
+                fontWeight: 600,
+                color: 'black',
+                textAlign: 'left',
+                marginTop: 0,
+                marginBottom: 0,
+              }}
+            >
+              {date.format('dddd')}
+            </Typography.Title>
+            <Typography.Text
+              className="fontFamily1"
+              style={{
+                fontSize: '12px',
+                color: 'black',
+                textAlign: 'left',
+                marginTop: 0,
+              }}
+            >
+              {date.format('MMMM DD, YYYY')}
+            </Typography.Text>
 
-                    <Col xs={3} style={{padding:20, display:'flex',justifyContent:'center',flexDirection:'column',alignItems:'center',borderRight:"1px solid #dadada"}}>
-                    <Typography.Title
-                className="fontFamily1"
-                style={{
-                  fontSize: "12px",
-                  fontWeight: 600,
-                  color: "black",
-                  textAlign: "left",
-                  marginTop: 0,
-                  marginBottom: 0,
-                }}
-              >
-                   Wednesday
-              </Typography.Title>
-              <Typography.Text
-                className="fontFamily1"
-                style={{
-                  fontSize: "12px",
-              
-                  color: "black",
-                  textAlign: "left",
-                  marginTop: 0,
-                }}
-              >
-                   March 24, 2023
-              </Typography.Text>
-                    </Col>
+      </div>
+        ))}
+      </AliceCarousel>
+  </Col>
+  <Col xs={0} md={1} className="flex" >
 
-                    <Col xs={3} style={{padding:20, display:'flex',justifyContent:'center',flexDirection:'column',alignItems:'center',borderRight:"1px solid #dadada"}}>
-                    <Typography.Title
-                className="fontFamily1"
-                style={{
-                  fontSize: "12px",
-                  fontWeight: 600,
-                  color: "black",
-                  textAlign: "left",
-                  marginTop: 0,
-                  marginBottom: 0,
-                }}
-              >
-                   Thursday
-              </Typography.Title>
-              <Typography.Text
-                className="fontFamily1"
-                style={{
-                  fontSize: "12px",
-              
-                  color: "black",
-                  textAlign: "left",
-                  marginTop: 0,
-                }}
-              >
-                   March 25, 2023
-              </Typography.Text>
-                    </Col>
+  <Button className="flex" shape="circle"  onClick={handleNextSlide}>
+  <FcNext style={{fontWeight:'bold'}}/>
+          </Button>
+  </Col>
+</Row>
+     
 
-                    <Col xs={3} style={{padding:20, display:'flex',justifyContent:'center',flexDirection:'column',alignItems:'center',borderRight:"1px solid #dadada"}}>
-                    <Typography.Title
-                className="fontFamily1"
-                style={{
-                  fontSize: "12px",
-                  fontWeight: 600,
-                  color: "black",
-                  textAlign: "left",
-                  marginTop: 0,
-                  marginBottom: 0,
-                }}
-              >
-                   Friday
-              </Typography.Title>
-              <Typography.Text
-                className="fontFamily1"
-                style={{
-                  fontSize: "12px",
-              
-                  color: "black",
-                  textAlign: "left",
-                  marginTop: 0,
-                }}
-              >
-                   March 26, 2023
-              </Typography.Text>
-                    </Col>
-
-                    <Col xs={3} style={{padding:20, display:'flex',justifyContent:'center',flexDirection:'column',alignItems:'center',borderRight:"1px solid #dadada"}}>
-                    <Typography.Title
-                className="fontFamily1"
-                style={{
-                  fontSize: "12px",
-                  fontWeight: 600,
-                  color: "black",
-                  textAlign: "left",
-                  marginTop: 0,
-                  marginBottom: 0,
-                }}
-              >
-                   Saturday
-              </Typography.Title>
-              <Typography.Text
-                className="fontFamily1"
-                style={{
-                  fontSize: "12px",
-              
-                  color: "black",
-                  textAlign: "left",
-                  marginTop: 0,
-                }}
-              >
-                   March 27, 2023
-              </Typography.Text>
-                    </Col>
-
-                    <Col xs={3} style={{padding:20, display:'flex',justifyContent:'center',flexDirection:'column',alignItems:'center'}}>
-                    <Typography.Title
-                className="fontFamily1"
-                style={{
-                  fontSize: "12px",
-                  fontWeight: 600,
-                  color: "black",
-                  textAlign: "left",
-                  marginTop: 0,
-                  marginBottom: 0,
-                }}
-              >
-                   Sunday
-              </Typography.Title>
-              <Typography.Text
-                className="fontFamily1"
-                style={{
-                  fontSize: "12px",
-              
-                  color: "black",
-                  textAlign: "left",
-                  marginTop: 0,
-                }}
-              >
-                   March 28, 2023
-              </Typography.Text>
-                    </Col>
+      
+        
 
 
-                </Row>
-                </div>
+   
+      
 
-                <br/>
-                <br/>
-
+              <br />
+              <br />
 
               <Typography.Title
                 className="fontFamily1"
@@ -275,31 +199,22 @@ function Calander() {
                   marginTop: 0,
                 }}
               >
-
-                   From
-
+                From
               </Typography.Title>
-              <br/>
+              <br />
 
               <Checkbox.Group
-                  className="avaliblityGroup"
-                  options={[
-                    "07 : 00 PM  to  08: 00 PM",
-                    "08 : 00 PM  to  09: 00 PM",
-                    "09 : 00 PM  to  10: 00 PM",
-                  ]}
-                 
-                  style={{
-                    display: "flex",
-                    columnGap: "50px",
-                  }}
-                />
-
-            
-
-
-
-            
+                className="avaliblityGroup"
+                options={[
+                  "07 : 00 PM  to  08: 00 PM",
+                  "08 : 00 PM  to  09: 00 PM",
+                  "09 : 00 PM  to  10: 00 PM",
+                ]}
+                style={{
+                  display: "flex",
+                  columnGap: "50px",
+                }}
+              />
             </Card>
           </div>
         </Col>
